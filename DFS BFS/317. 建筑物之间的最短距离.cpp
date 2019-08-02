972d40a69ef91197d9578bcce6928b207c10eb5d
apple <apple@appledeMacBook-Pro.local>
2019-08-02 22:21:09 +0800

    更新了新解法

--- DFS BFS/317. 建筑物之间的最短距离.cpp
+++ DFS BFS/317. 建筑物之间的最短距离.cpp
@@ -18,14 +18,85 @@
     问题分析：
         对每个非0元素进行BFS来计算到达所有建筑物距离的总和，
         注意，这个总和必须到达所有建筑物，缺一个都不行。
+        
+        ---分割线---
+        如果是对0来进行bfs，会超时，因为0很多！
+        而1是很少的，故应该反向过来，对1进行bfs
+        对每一个0的位置(i,j)：
+            dis[i][j]保存了所有1到当前0的距离总和
+            计算能到达当前0的1的个数，即canReach[i][j]
+        用1去bfs来更新上面的dis和canReach.
  */
 
- class Solution {
+
+class Solution {
 public:
     /**
      * @param grid: the 2D grid
      * @return: the shortest distance
      */
+    int shortestDistance(vector<vector<int>> &grid) {
+        // write your code here
+        if ( grid.size()==0 || grid[0].size()==0 ) return 0;
+        int n=grid.size(), m=grid[0].size();
+        
+        // dis[i][j]表示当前位置到所有1的距离和
+        // canReach[i][j] 表示当前位置能够到达的building数 
+        vector<vector<int>> dis(n, vector<int>(m, 0));
+        vector<vector<int>> canReach(n, vector<int>(m, 0));
+        int res = INT_MAX, cnt=0;
+        for ( int i=0; i<grid.size(); ++i )
+            for ( int j=0; j<grid[0].size(); ++j )
+                if ( grid[i][j]==1 ){
+                    calculate_distance(dis, canReach, i, j, grid, cnt);
+                    ++cnt;
+                }
+        res = INT_MAX;
+        for ( int i=0; i<grid.size(); ++i )
+            for ( int j=0; j<grid[0].size(); ++j )
+                if ( grid[i][j]==0 && canReach[i][j]==cnt )
+                    res = min(dis[i][j], res);
+        
+        return res==INT_MAX?-1:res;
+    }
+    
+    void calculate_distance( vector<vector<int>> & dis, vector<vector<int>> & canReach, int i, int j, vector<vector<int>>& grid, int cnt ){
+        int step=0, n=grid.size(), m=grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<bool>> visited( n, vector<bool>(m,false));
+        q.push({i,j});
+        visited[i][j] = true;
+        int dir[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+        while( !q.empty() ){
+            int len = q.size();
+            for ( int t=0; t<len; ++t ){
+                pair<int,int> cor = q.front(); q.pop();
+                for ( int k=0; k<4; ++k ){
+                    int y = cor.first + dir[k][0];
+                    int x = cor.second +dir[k][1];
+                    if ( x>=0 && y>=0 && x<m && y<n && !visited[y][x] && grid[y][x]==0 ){
+                        //访问过当前位置
+                        visited[y][x] = true;
+                        //更新dis
+                        dis[y][x] += (step + 1);
+                        //更新canReach
+                        canReach[y][x] += 1;
+                        q.push({y,x});
+                    }
+                }
+            }
+            // step为从i，j到达当前位置需要的步数，初始为0
+            ++step;
+        }
+        return;
+    }
+};
+
+/* 
+
+TLE
+class Solution {
+public:
     int shortestDistance(vector<vector<int>> &grid) {
         // write your code here
         int res = INT_MAX, cnt=0;
@@ -76,4 +147,4 @@ public:
         return c==cnt?res:INT_MAX;
     }
     
-};
+};*/
