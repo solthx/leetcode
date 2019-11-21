b7e99dc07163ad46be65dfe88216ed5cf978fd68
czf <1154238323@qq.com>
2019-11-21 16:32:54 +0800

    条件限制的BFS

--- DFS BFS/1263. 推箱子.cpp
+++ DFS BFS/1263. 推箱子.cpp
@@ -59,5 +59,81 @@
 
 
     问题分析:
-        本题在普通的bfs上加了一些限制， 同时对bfs时，需要保存两个状态，即箱子的位置和人的
+        本题在普通的bfs上加了一些限制， 同时对bfs时，需要保存两个状态，即箱子的位置和人的位置.
+        在进行bfs时， 寻找下一个位置的时候，要满足下面几个条件(假设下一个箱子的位置是(y,x), 下一个人的位置是(ry, rx)):
+            1. rx,ry,y,x不越界
+            2. 这个位置状态曾经为抵达过(用vis数组保存,即使vis[ry][rx][y][x])
+            3. 移动方向的反方向的位置是可以站人的( 例如 从(1,1)移动到了(1,2)， 那么就需要玩家站在(1,0)的位置推，因此(1,0)就是反方向位置 )
+            4. 移动方向的反方向的位置是当前玩家可以抵达的 ( 这个再通过一个bfs来判断是否可达就行了。 )
+
+        设计好上面的思路，实现起来虽然略繁琐， 但并不难。
+
+        本题的两个收获:
+            1. 先想清楚，再coding! 边coding边想思路是大忌！！！！
+            2. vis数组保存状态的时候，不要仅仅拘泥于保存一个位置！！ 可以保存多个位置，或位置+其他状态的组合。。总之vis数组依然是保存状态的， 位置只是状态的一种。
 */
+
+class Solution {
+public:
+    int minPushBox(vector<vector<char>>& grid) {
+        queue<vector<int>> q;
+        int vis[30][30][30][30] = {0};
+        int n = grid.size(), m=grid[0].size(),sx,sy;
+        for ( int i=0; i<n; ++i )
+            for ( int j=0; j<m; ++j )
+                if ( grid[i][j]=='B' ){
+                    q.push({i,j});
+                }else if (grid[i][j]=='S'){
+                    sy = i;
+                    sx = j;
+                }
+        vis[q.front()[0]][q.front()[1]][sy][sx]=1;
+        q.push({q.front()[0], q.front()[1], sy, sx});
+        q.pop();
+
+        int res = 0;
+        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+        while( !q.empty() ){
+            for ( int j=q.size(); j>0; --j ){
+                auto cur = q.front(); q.pop();
+                for ( int k=0; k<4; ++k ){
+                    int y = cur[0] + dir[k][0];
+                    int x = cur[1] + dir[k][1];
+                    int ry = cur[0] - dir[k][0];
+                    int rx = cur[1] - dir[k][1];
+                    if ( x>=0 && rx>=0 &&y>=0 &&ry>=0 && x<m && rx<m && y<n && ry<n&& vis[y][x][ry][rx]==0 && grid[ry][rx]!='#' && grid[y][x]!='#' && canReach( grid, ry, rx, cur[2], cur[3], cur[0],cur[1] ) ){
+                        vis[y][x][ry][rx] =1;
+                        if ( grid[y][x] == 'T' )
+                            return res+1;
+                        q.push({y,x,ry,rx});
+                    }
+                }
+            }
+            ++res;
+        }
+        return -1;
+    }
+
+    // 判断是否可达
+    bool canReach( vector<vector<char>>& grid, int y, int x, int ty, int tx, int fobidy, int fobidx  ){
+        queue<vector<int>> q;
+        int vis[30][30] = {0};
+        q.push({y,x});
+        vis[y][x] = 1;
+        vis[fobidy][fobidx] = 1;
+        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+        while( q.size()>0 ){
+            auto cur = q.front(); q.pop();
+            for ( int k=0; k<4; ++k ){
+                int ny = cur[0] + dir[k][0];
+                int nx = cur[1] + dir[k][1];
+                if ( ny>=0 && nx>=0 &&ny<grid.size() && nx<grid[0].size() && vis[ny][nx]==0 && grid[ny][nx] != '#' ){
+                    if ( ny==ty && nx==tx ) return true;
+                    vis[ny][nx] = true;
+                    q.push({ny,nx});
+                }
+            }
+        }
+        return false;
+    }
+};
